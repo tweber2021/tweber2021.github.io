e4d45529bb1176a895c38e8c342d7d6d41f857cc
tweber2021 <42779851+tweber2021@users.noreply.github.com>
2019-01-14 21:26:23 -0500

    Behind the scenes gravity calculations

--- particles/particles.js
+++ particles/particles.js
@@ -7,9 +7,11 @@ canvas.height = document.body.clientHeight;
 var ctx=canvas.getContext("2d");
 
 // Init. Variables
-const n = 2000;
+// assumed: m=1
+const n = 1000;
 const w = canvas.width;
 const h = canvas.height;
+const G = 1000000; // Gravitational constant
 var x = [];
 var y = [];
 var vx = [];
@@ -27,22 +29,34 @@ for(i=0;i<n;i++){
 // Run
 setInterval(update,1000/60); // Update at 60 fps
 
-function update(){
+function applyForces(p){ // Calculate and apply normal and gravitational forces
+	// Calculate and apply all gravitational forces on p
+	var d;
+	for(i=0;i<n;i++){
+		d = Math.sqrt(Math.pow(x[p]-x[i],2)+Math.pow(y[p]-y[i],2));
+		Fg = G*(1/Math.pow(d,2));
+		// TODO: Trig
+	}
+	// TODO: Apply normal force if needed
+}
+
+function update(){ // Update simulation
 	var j;
 	for(j=0;j<n;j++){
 		erase(x[j],y[j]);
+		applyForces(j);
 		x[j] += vx[j];
 		y[j] += vy[j];
 		plot(x[j],y[j])
 	}
 }
 
-function plot(x,y){
+function plot(x,y){ // Fill in a pixel
 	ctx.fillStyle = "white";
 	ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
 
-function erase(x,y){
+function erase(x,y){ // Erase a pixel
 	ctx.fillStyle = "black";
 	ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
