018737179c8f29c6affcd4b19dedb7ae318610ad
tweber2021 <42779851+tweber2021@users.noreply.github.com>
2019-01-15 21:18:19 -0500

    Made gravity work, pseudo-normal force

--- particles/particles.js
+++ particles/particles.js
@@ -7,9 +7,12 @@ canvas.height = document.body.clientHeight;
 var ctx=canvas.getContext("2d");
 
 // Init. Variables
-const n = 1000;
+// assumed: m=1
+const n = 500;
 const w = canvas.width;
 const h = canvas.height;
+const Vmax = 8;
+const G = 100; // Fake gravitational constant
 var x = [];
 var y = [];
 var vx = [];
@@ -20,29 +23,46 @@ var i;
 for(i=0;i<n;i++){
 	x[i] = Math.random()*w;
 	y[i] = Math.random()*h;
-	vx[i] = (Math.random()*4)-2;
-	vy[i] = (Math.random()*4)-2;
+	vx[i] = 0;//(Math.random()*4)-2;
+	vy[i] = 0;//(Math.random()*4)-2;
 }
 
 // Run
-setInterval(update,1000/60); // Update at 60 fps
+setInterval(update,1); // Update at 60 fps
 
-function update(){
+function applyForces(p){ // TODO: Calculate and apply normal and gravitational forces
+	// Calculate and apply all gravitational forces on p
+	for(i=0;i<n;i++){
+		if(i===p){continue;}
+		var dx = x[p]-x[i];
+		var dy = y[p]-y[i];
+		var d = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+		var Fg = G/Math.pow(d,2);
+		var theta = Math.atan(Math.abs(dy)/Math.abs(dx));
+		vx[p] += -1*Math.sign(dx)*Fg*Math.cos(theta);
+		vy[p] += -1*Math.sign(dy)*Fg*Math.sin(theta);
+		if(Math.abs(vx[p])>Vmax){vx[p]=0;}
+		if(Math.abs(vy[p])>Vmax){vy[p]=0;} // Fake normal force
+	}
+}
+
+function update(){ // Update simulation
 	var j;
 	for(j=0;j<n;j++){
 		erase(x[j],y[j]);
+		applyForces(j);
 		x[j] += vx[j];
 		y[j] += vy[j];
 		plot(x[j],y[j])
 	}
 }
 
-function plot(x,y){
+function plot(x,y){ // Fill in a pixel
 	ctx.fillStyle = "white";
 	ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
 
-function erase(x,y){
+function erase(x,y){ // Erase a pixel
 	ctx.fillStyle = "black";
 	ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
 }
