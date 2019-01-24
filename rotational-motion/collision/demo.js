b9ec5df52fddc94116ff675e7ada31eab56c301b
tweber2021 <tweber2021@users.noreply.github.com>
2019-01-24 08:42:29 -0500

    Basic, approximated collision

--- rotational-motion/collision/demo.js
+++ rotational-motion/collision/demo.js
@@ -29,7 +29,7 @@ var body = [
 		ay: 0,
 		angP: 0,
 		angV: 0,
-		angA: 0.0002
+		angA: 0.00005
 	},
 	{
 		cx: 500,
@@ -46,7 +46,7 @@ var body = [
 		ay: 0,
 		angP: 0,
 		angV: 0,
-		angA: 0.0002
+		angA: 0.00005
 	}
 ];
 
@@ -81,7 +81,7 @@ function update(){ // Update simulation
 		}
 		
 		// Collision
-		colBroad(body[0],body[1]);
+		chkCollision(body[0],body[1]);
 		
 		draw();
 	}
@@ -142,11 +142,36 @@ function updateText(){
 	ap2.innerHTML = "α₂ = "+body[1].angA+" rads/frame²"
 }
 
-function colBroad(b1,b2){ // Broad-phase collision detection
+function chkCollision(b1,b2){
+	if(colBroad(b1,b2)){
+		colNarrow(b1,b2);
+		b1.vx*=-1;
+		b1.angV*=-1;
+		b2.vx*=-1;
+		b2.angV*=-1
+	}
+	return false;
+}
+
+function colBroad(b1,b2){ // Broad-phase collision detection - return whether two bodies might be colliding
 	var lim1 = getBounds(b1);
 	var lim2 = getBounds(b2);
-	rec(lim1[0]+ofsX,lim1[2]+ofsY,lim1[1]+ofsX,lim1[3]+ofsY);
-	rec(lim2[0]+ofsX,lim2[2]+ofsY,lim2[1]+ofsX,lim2[3]+ofsY);
+	//rec(lim1[0]+ofsX,lim1[2]+ofsY,lim1[1]+ofsX,lim1[3]+ofsY);
+	//rec(lim2[0]+ofsX,lim2[2]+ofsY,lim2[1]+ofsX,lim2[3]+ofsY);
+	return chkRectCol(lim1[0],lim1[2],lim1[1],lim1[3],lim2[0],lim2[2],lim2[1],lim2[3]);
+}
+
+function colNarrow(b1,b2){ // Kind of broken, seemingly
+	var i;
+	var j;
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			if(chkLineCol(b1.Rx[i],b1.Ry[i],b1.Rx[(i+1)%4],b1.Ry[(i+1)%4],b2.Rx[i],b2.Ry[i],b2.Rx[(i+1)%4],b2.Ry[(i+1)%4])){
+				console.log("narrow collision!");
+				return true;
+			}
+		}
+	}
 	return false;
 }
 
@@ -159,6 +184,27 @@ function getBounds(bdy){ // Get the top left corner for broad-phase detection
 		if(bdy.Ry[i]<lim[2] || lim[2] == undefined){lim[2] = bdy.Ry[i];}
 		if(bdy.Ry[i]>lim[3] || lim[3] == undefined){lim[3] = bdy.Ry[i];}
 	}
-	console.log(lim[1]-lim[0],lim[3]-lim[2]);
 	return lim;
+}
+
+function chkLineCol(x1,y1,x2,y2,x3,y3,x4,y4){ // Detect collision between two lines
+	var denominator = ((x2 - x1) * (y4 - y3)) - ((y2 - y1) * (x4 - x3));
+    var numerator1 = ((y1 - y3) * (x4 - x3)) - ((x1 - x3) * (y4 - y3));
+    var numerator2 = ((y1 - y3) * (x2 - x1)) - ((x1 - x3) * (y2 - y1));
+
+    // Detect coincident lines
+    if (denominator == 0){return numerator1 == 0 && numerator2 == 0;}
+
+    var r = numerator1 / denominator;
+    var s = numerator2 / denominator;
+
+    return (r >= 0 && r <= 1) && (s >= 0 && s <= 1);
+}
+
+function chkRectCol(x1,y1,x2,y2,x3,y3,x4,y4){
+	var w1 = x2-x1;
+	var h1 = y2-y1;
+	var w2 = x4-x3;
+	var h2 = y4-y3;
+	return x1<x4 && x2>x3 && y1<y4 && y2>y3;
 }
